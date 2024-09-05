6355ee62730d17f06c18c2048d510cbecf1999b8
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-09-05 10:53:11 +0200

    Integer operations

---
+++ IntegerOperations.java
@@ -0,0 +1,24 @@
+public class IntegerOperations {
+
+
+//    Add two integers using Integer wrapper class (autoboxing).
+public static void sum(Integer a, Integer b) {
+    System.out.println("sum: " + (a + b));
+}
+
+//    Subtract two integers using Integer wrapper class (unboxing).
+public static void subtraction(int a, int b) {
+    System.out.println("subtraction: " + (a - b));
+}
+
+//    Multiply two integers using Integer wrapper class (autoboxing).
+public static void multiplication(Integer a, Integer b) {
+    System.out.println("multiplication: " + (a * b));
+}
+
+//    Divide two integers using Integer wrapper class (unboxing).
+public static void division(int a, int b) {
+    System.out.println("division: " + (a + b));
+}
+
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
 public class Main{
 
+    public static void main(String[] args) {
+
+
+//        Add two integers using Integer wrapper class (autoboxing).
+        IntegerOperations.sum(1, 3);
+
+//        Subtract two integers using Integer wrapper class (unboxing).
+        Integer a = 3, b = 5;
+        IntegerOperations.subtraction(a, b);
+
+//        Multiply two integers using Integer wrapper class (autoboxing).
+        IntegerOperations.multiplication(1, 3);
+
+//        Divide two integers using Integer wrapper class (unboxing).
+        IntegerOperations.division(a, b);
+
+    }
+
 }
